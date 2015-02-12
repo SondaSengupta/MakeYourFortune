7bf2f83fbd32d3afc549cc78cef36c7cce7f2d09
SondaSengupta <sondasengupta@gmail.com>
2015-02-12 13:09:39 -0600

    Implement classes to test viewing fortunes

---
+++ FortuneTest/FortuneItemModelTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MakeYourFortune.Model;
+
+namespace FortuneTest
+{
+    [TestClass]
+    public class FortuneItemModelTest
+    {
+        [TestMethod]
+        public void CreatingFortuneStoresitsProperties()
+        {
+            FortuneItem kidnapped = new FortuneItem("Help! I'm being forced to work in a Chinese Bakery", "Relationships");
+            Assert.AreEqual("Help! I'm being forced to work in a Chinese Bakery", kidnapped.FortuneText);
+            Assert.AreEqual("Relationships", kidnapped.FortuneCategory);
+        }
+    }
+}
---
+++ MakeYourFortune/Model/FortuneItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeYourFortune.Model
+{
+    public class FortuneItem
+    {
+        public string FortuneText;
+        public string FortuneCategory;
+
+        public FortuneItem(string FortuneText, string FortuneCategory)
+        {
+            this.FortuneText = FortuneText;
+            this.FortuneCategory = FortuneCategory;
+        }
+    }
+}
